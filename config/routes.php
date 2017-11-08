61d084090f6b028eaba612d9ecbfee2ccab45359
Javed Wright <jdwright870@gmail.com>
2017-11-08 07:15:07 +0000

    added favicon.ico route

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@ $routes = ["index.php" => "controller/index.controller.php",
 "home" => "controller/home.controller.php",
 'login' => "views/login.view.php",
 'log' => 'controller/login.controller.php',
+'favicon.ico' => 'favicon.ico',
 "404" => "views/error404.view.php"
 ];
 
