4d1304bc83f09642d5518124d5999000c05ba136
Javed Wright <jdwright870@gmail.com>
2017-11-10 06:17:05 +0000

    view updated.

--- views/login.view.php
+++ views/login.view.php
@@ -1,6 +1,16 @@
 <?php require 'partials/header.html'?>
-<form action="log" method="POST">
-    <input type="text" name="username"/>
-    <input type="password" name="pass"/>
-    <input type="submit" value="Submit"/>
-</form>
+<link rel="stylesheet" type="text/css" href="login.css" />
+<div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>Cheapo<span>Mail</span></div>
+		</div>
+		<br>
+		<form action="log" method="POST">
+		<div class="login">
+			<input type="text" placeholder="username" name="username"><br>
+			<input type="password" placeholder="password" name="pass"><br>
+			<input type="submit" value="Login">
+		</div>
+		</form>
+
