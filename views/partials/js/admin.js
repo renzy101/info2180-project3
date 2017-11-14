0209c690758bc118890cc0e2de82ca0421931df0
Javed Wright <jdwright870@gmail.com>
2017-11-14 04:28:10 +0000

    added ajax request.

--- views/partials/js/admin.js
+++ views/partials/js/admin.js
@@ -94,7 +94,7 @@ $(document).ready(function(){
                      alert(response);
                      clearFields();
                  },
-                 failure:function(response){
+                 failure: function(response){
                      alert(response);
                  }
             });
