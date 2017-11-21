96ce93ce853d6f1a5ab65b17ba633c5d6dd32feb
Javed Wright <jdwright870@gmail.com>
2017-11-21 23:56:04 +0000

    read model.

---
+++ controller/read.controller.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_SESSION['user'])){
+    if(!empty($_POST['msgid'])){
+        require 'controller/sanitation.php';
+        require 'models/messaging.model.php';
+        
+        $msgid = sanitation(['msgid' => $_POST['msgid']]);
+        
+        $messagingMOD = new MessagingModel();
+        
+        if($messagingMOD->is_read($msgid['msgid']) == false){
+            if($messagingMOD->markMessageRead($msgid['msgid'])){
+                echo 1;
+                die();
+            }
+            echo 0;
+            die();
+        }
+        echo 1;
+        die();
+    }
+    echo -1;
+    die();
+}else{
+    header('Location: /');
+}
